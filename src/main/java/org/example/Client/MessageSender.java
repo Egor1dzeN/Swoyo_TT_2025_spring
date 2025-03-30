a7aa84d1320bdd0940c32213f89b6d0a8948cf0d
Egor1dzeN <egormorozov123450@gmail.com>
2025-03-30 10:03:47 +0300

    add: class for sending messages

--- src/main/java/org/example/Client/MessageSender.java
+++ src/main/java/org/example/Client/MessageSender.java
@@ -1,8 +1,27 @@
 package org.example.Client;
 
+import io.netty.channel.Channel;
 import lombok.Data;
+import org.example.Client.DTO.ObjectDTO;
+
+import java.util.concurrent.CountDownLatch;
 
 @Data
 public class MessageSender {
-    private String username;
+    private ObjectDTO objectDTO;
+    private Channel channel;
+
+    public MessageSender(ObjectDTO objectDTO, Channel channel) {
+        this.objectDTO = objectDTO;
+        this.channel = channel;
+    }
+
+    public void sendMessage(CountDownLatch latch) {
+        channel.writeAndFlush(objectDTO).addListener(future -> {
+            if (!future.isSuccess()) {
+                System.err.println("Ошибка отправки: " + future.cause());
+                latch.countDown();
+            }
+        });
+    }
 }
