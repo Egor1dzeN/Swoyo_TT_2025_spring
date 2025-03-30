a34da6b9fc321c3ffcb3051c345f4ca6f388b9ad
Egor1dzeN <egormorozov123450@gmail.com>
2025-03-30 10:02:40 +0300

    add: ObjectDTO

---
+++ src/main/java/org/example/Client/DTO/CommandType.java
@@ -0,0 +1,9 @@
+package org.example.Client.DTO;
+
+import java.io.Serializable;
+
+public enum CommandType implements Serializable {
+    CREATE_TOPIC,
+    VIEW,
+    CREATE_VOTE,
+}
--- src/main/java/org/example/Client/DTO/ObjectDTO.java
+++ src/main/java/org/example/Client/DTO/ObjectDTO.java
@@ -1,15 +1,28 @@
 package org.example.Client.DTO;
 
 import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.io.Serializable;
+import java.util.HashMap;
 
 @Getter
-public class ObjectDTO {
+@ToString
+@Setter
+public class ObjectDTO implements Serializable {
     private String username;
     private CommandType commandType;
+    private HashMap<String, String> params;
     private Object data;
-    enum CommandType {
-        CREATE_TOPIC,
-        VIEW,
-        CREATE_VOTE,
+
+    public ObjectDTO(String username, Object data, HashMap<String, String> params, CommandType commandType) {
+        this.username = username;
+        this.data = data;
+        this.params = params;
+        this.commandType = commandType;
+    }
+
+    public ObjectDTO() {
     }
 }
